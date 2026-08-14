835ab7dddb9b2d1bc9d8782fadd056f973de30b0
Matt Sarno <matt.sarno@pm.me>
2026-08-14 17:47:13 -0600

    schema: restrict memory_entries FK on subject delete

---
+++ migrations/0004_restrict_memory_entries_subject_delete.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE memory_entries DROP CONSTRAINT memory_entries_subject_id_fkey;
+ALTER TABLE memory_entries ADD CONSTRAINT memory_entries_subject_id_fkey
+    FOREIGN KEY (subject_id) REFERENCES subjects(slug) ON DELETE RESTRICT;
